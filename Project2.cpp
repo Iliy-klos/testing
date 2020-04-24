6806638b96dd056b9e38e9da14d55e14e3cedb79
43245556+090101ikn@users.noreply.github.com <090101ikn>
2020-04-24 19:15:10 +0300

    gj

--- Project2.cpp
+++ Project2.cpp
@@ -5,7 +5,6 @@
 #include <tchar.h>
 //---------------------------------------------------------------------------
 USEFORM("Path.cpp", Form4);
-USEFORM("Put.cpp", Form5);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -14,7 +13,6 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
 		Application->CreateForm(__classid(TForm4), &Form4);
-		Application->CreateForm(__classid(TForm5), &Form5);
 		Application->Run();
 	}
 	catch (Exception &exception)
